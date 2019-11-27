0ca52b1b3469cf70905ce7ecb3ce0470500ef16c
Luis Felipe Mileo <mileo@kmee.com.br>
2019-11-27 13:30:33 -0300

    [NEW] Emissão através do webservice da ginfes v3.01

---
+++ nfselib/ginfes/v3_01/generatedsnamespaces.py
@@ -0,0 +1,8 @@
+
+GenerateDSNamespaceDefs = {
+    "ConsultarLoteRpsEnvio": 'xmlns="http://www.ginfes.com.br/servico_consultar_lote_rps_envio_v03.xsd" xmlns:tipos="http://www.ginfes.com.br/tipos_v03.xsd"',
+    "EnviarLoteRpsEnvio": 'xmlns="http://www.ginfes.com.br/servico_enviar_lote_rps_envio_v03.xsd" xmlns:tipos="http://www.ginfes.com.br/tipos_v03.xsd"',
+    "ListaRpsType": 'xmlns="http://www.ginfes.com.br/tipos_v03.xsd"',
+    "ConsultarSituacaoLoteRpsEnvio": 'xmlns="http://www.ginfes.com.br/servico_consultar_situacao_lote_rps_envio_v03.xsd" xmlns:tipos="http://www.ginfes.com.br/tipos_v03.xsd"',
+    "cabecalho": 'xmlns:ns2="http://www.ginfes.com.br/cabecalho_v03.xsd"',
+}
