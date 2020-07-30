d957e3b3c184bb1f1760f3628e05f877deab4bd2
Luis Malta <luis.malta@kmee.com.br>
2020-07-30 11:51:38 -0300

    [ADD] CancelarNfseEnvio GenerateDSNamespace

--- nfselib/ginfes/v3_01/generatedsnamespaces.py
+++ nfselib/ginfes/v3_01/generatedsnamespaces.py
@@ -5,4 +5,6 @@ GenerateDSNamespaceDefs = {
     "ListaRpsType": 'xmlns="http://www.ginfes.com.br/tipos_v03.xsd"',
     "ConsultarSituacaoLoteRpsEnvio": 'xmlns="http://www.ginfes.com.br/servico_consultar_situacao_lote_rps_envio_v03.xsd" xmlns:tipos="http://www.ginfes.com.br/tipos_v03.xsd"',
     "cabecalho": 'xmlns:ns2="http://www.ginfes.com.br/cabecalho_v03.xsd"',
+    "CancelarNfseEnvio": 'xmlns:p="http://www.ginfes.com.br/servico_cancelar_nfse_envio_v03.xsd" xmlns:p1="http://www.ginfes.com.br/tipos_v03.xsd"',
+
 }
