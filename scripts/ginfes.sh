940e22e36e754dc8c1f7a1cc99b5cf86c70791ee
Luis Felipe Mileo <mileo@kmee.com.br>
2020-10-05 17:28:06 -0300

    [NEW] Regeração da lib ginfes
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ scripts/ginfes.sh
@@ -0,0 +1,4 @@
+erpbrasil-edoc-gen-download-schema -n ginfes -v v3.01 -u https://itajuba.ginfes.com.br/nfseweb/download/schemas_v301Abrasf.zip
+erpbrasil-edoc-gen-generate-python -n ginfes -v v3.01 -i "servico_enviar_lote_rps_resposta_v03|servico_consultar_situacao_lote_rps_resposta_v03|servico_consultar_lote_rps_resposta_v03|servico_enviar_lote_rps_envio_v03" -d .
+# O servico_enviar_lote_rps_envio_v03 foi necessário pois a classe EnviarLoteRpsEnvio
+# não estava no arquivo de retorno.
