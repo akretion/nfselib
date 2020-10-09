50908439d0de8e602c7696b80faa5876f5d2d7c4
Luis Malta <luis.malta@kmee.com.br>
2020-10-09 07:12:26 -0300

    [FIX] ginfes generation: missing class consultar_resposta and cancelar_resposta

--- scripts/ginfes.sh
+++ scripts/ginfes.sh
@@ -1,4 +1,4 @@
 erpbrasil-edoc-gen-download-schema -n ginfes -v v3.01 -u https://itajuba.ginfes.com.br/nfseweb/download/schemas_v301Abrasf.zip
-erpbrasil-edoc-gen-generate-python -m nfse -n ginfes -v v3.01 -i "servico_enviar_lote_rps_resposta_v03|servico_consultar_situacao_lote_rps_resposta_v03|servico_consultar_lote_rps_resposta_v03|servico_enviar_lote_rps_envio_v03|servico_consultar_situacao_lote_rps_envio_v03|servico_consultar_lote_rps_envio_v03|servico_cancelar_nfse_envio_v03|servico_consultar_nfse_rps_envio_v03|cabecalho_v03" -d .
+erpbrasil-edoc-gen-generate-python -m nfse -n ginfes -v v3.01 -i "servico_enviar_lote_rps_resposta_v03|servico_consultar_situacao_lote_rps_resposta_v03|servico_consultar_lote_rps_resposta_v03|servico_enviar_lote_rps_envio_v03|servico_consultar_situacao_lote_rps_envio_v03|servico_consultar_lote_rps_envio_v03|servico_cancelar_nfse_envio_v03|servico_consultar_nfse_rps_envio_v03|cabecalho_v03|servico_consultar_nfse_resposta_v03|servico_cancelar_nfse_resposta_v03" -d .
 # O servico_enviar_lote_rps_envio_v03 foi necessário pois a classe EnviarLoteRpsEnvio
 # não estava no arquivo de retorno.
